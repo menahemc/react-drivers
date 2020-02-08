69be755bd156135a01b548690aa550a63c442699
unknown <menachem1988@gmail.com>
2020-02-09 00:00:45 +0200

    fiter drivers

--- src/components/driver.js
+++ src/components/driver.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 
-
 export default class Driver extends React.Component {
 
+    getImageSrc() {
+        return this.props.driver.profile_image ? this.props.driver.profile_image : "https://i.imgur.com/S2IRHcz.jpg";
+    }
 
 
     render() {
         return (
-            <div>
-                {this.props.driver.phone}
-        </div>
+            <div className="driver">
+                <div className="driver__image-container">
+                    <img className="driver__image-container--img" src={this.getImageSrc()} alt="" />
+                </div>
+                <div className="driver__details-container">
+                    <div className="driver__details-container--full-name">{this.props.driver.name}</div>
+                    <div className="driver__details-container--rank">{this.props.driver.driverRank}</div>
+                    <div>
+                        <span>Phone Number:</span>
+                        <span>{this.props.driver.phone}</span>
+                    </div>
+                    <div>
+                        <span>Email:</span>
+                        <span>{this.props.driver.email}</span>
+                    </div>
+                    <img className="driver__details-container--type-icon"
+                        src={require("../pictures/" + this.props.driver.driverType.trim().toLocaleLowerCase() + ".svg")} alt="" />
+                </div>
+            </div>
         );
     }
 }
--- src/components/drivers.js
+++ src/components/drivers.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Driver from "./driver"
 
-
-
 export default class Drivers extends React.Component {
 
     state = {
-        drivers: []
+        drivers: [],
+        filteredDrivers: []
     }
 
     componentDidMount() {
@@ -15,17 +14,40 @@ export default class Drivers extends React.Component {
             .then(res => {
                 const drivers = res.data;
                 this.setState({ drivers });
-                console.log(this.state);
+                this.setState({ filteredDrivers: drivers });
             })
     }
 
+    filterItems = (event) => {
+        let keys = ["name", "driverType", "driverRank", "phone", "email"];
+        let term = event.target.value;
+        let filteredDrivers = (this.state.drivers || []).filter(item => keys.some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])));
+        this.setState({ filteredDrivers });
+    }
+
+    header() {
+        return (
+            <div className="header">
+                <div className="header__title">Contact List</div>
+                <div className="header__search-container">
+                    <input onChange={this.filterItems} className="header__search-container--input" type="text" placeholder="Search..." />
+                </div>
+            </div>
+        )
+    }
 
     render() {
         return (
-            <div>{
-                this.state.drivers.map((dr, i) => <Driver key={i} driver={dr} />)
-            }
+            <div>
+                {this.header()}
+                <div className="drivers">
+                    <div className="drivers__wrapper">
+                        <div className="drivers__wrapper--container">
+                            {this.state.filteredDrivers.map((dr, i) => <Driver key={i} driver={dr} />)}
+                        </div>
+                    </div>
+                </div>
             </div>
-        );
+        )
     }
 }
